9e94f1ab9dc673be9fa6bd7a4aba03edbef9b681
María Victoria Doll <mvictoriadoll@gmail.com>
2025-06-24 19:29:24 +0200

    Add confirmation button to VerifyPage

--- client/src/components/VerifyPage.tsx
+++ client/src/components/VerifyPage.tsx
@@ -1,19 +1,19 @@
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import './VerifyPage.css';
 
-export default function VerifyPage () {
+export default function VerifyPage() {
   const [items, setItems] = useState([
     {
-      id:'1',
+      id: '1',
       originalBillLabel: 'Chocapic',
       aiSuggestedName: 'Cereal',
       price: 4.5,
       isFoodItem: true,
       classification: 'High Sugar',
-    }, 
+    },
     {
-      id:'2',
+      id: '2',
       originalBillLabel: 'Pepsi Max',
       aiSuggestedName: 'Soda',
       price: 3.0,
@@ -22,34 +22,34 @@ export default function VerifyPage () {
 
     }
   ]);
-  //const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  /*const handleConfirm = () =>   {
+  const handleConfirm = () =>   {
     navigate('/dashboard');
-  }*/
-  
-    const toggleIsFood=(id:string) => {
+  }
 
-      const updatedItems = items.map(item => 
-        item.id === id ? {...item, isFoodItem: !item.isFoodItem}: item
-      );
-      setItems(updatedItems);
+  const toggleIsFood = (id: string) => {
 
-    };
+    const updatedItems = items.map(item =>
+      item.id === id ? { ...item, isFoodItem: !item.isFoodItem } : item
+    );
+    setItems(updatedItems);
+
+  };
 
 
-  return(
+  return (
     <section className='verify-page'>
       <h2 className='verify-title'>Verify your Items</h2>
       <ul className='verify-list'>
-        {items.map(item =>(
+        {items.map(item => (
           <li key={item.id} className='verify-item'>
             <p className='verify-label'>Original label: {item.originalBillLabel}</p>
             <p className='verify-suggestion'>AI suggestion: {item.aiSuggestedName}</p>
             <p className='verify-price'>Price: ${item.price}</p>
             <p className='verify-food'>Is Food: {''}
               <button className='verify-toggle-btn' onClick={() => toggleIsFood(item.id)}>
-                {item.isFoodItem ? '✅': '❌'}
+                {item.isFoodItem ? '✅' : '❌'}
               </button>
             </p>
             <p className='verify-category'>Category: {item.classification}</p>
@@ -57,8 +57,11 @@ export default function VerifyPage () {
           </li>
         ))}
       </ul>
+      <button className="verify-confirm-button" onClick={handleConfirm}>
+        Confirm and continue
+      </button>
 
     </section>
-  
+
   );
 }
