d132a1b4675a45afe91f996f5591bb104b29a429
Shejo Joseph <shejojoseph@gmail.com>
2025-06-24 21:50:54 +0200

    server update 24/06/2025: includes AI logic and database insertion.

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,17 +2,25 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 // Load environment variables from .env file
-dotenv.config({path: path.resolve(__dirname, '../../.env')});
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-const config ={
+const config = {
   port: process.env.PORT || 3000,
   databaseUrl: process.env.DATABASE_URL as string,
-  frondendBuildPath: path.join(__dirname, '..','..','..','client', 'dist'),
+  frondendBuildPath: path.join(__dirname, '..', '..', '..', 'client', 'dist'),
   nodeEnv: process.env.NODE_ENV || 'development',
+  //Langchain Google integration
+  googleApiKey: process.env.GOOGLE_API_KEY as string,
+  geminiModelName: process.env.GEMINI_MODEL_NAME || 'gemini-2.5-flash',
 };
 
 if (!config.databaseUrl) {
   console.error('FATAL ERROR: DATABASE_URL is not defined in .env');
+  process.exit(1);
+}
+
+if (!config.googleApiKey) {
+  console.error('FATAL ERROR: GOOGLE_API_KEY is not defined in .env. Required for AI processing.');
   process.exit(1);
 }
 
