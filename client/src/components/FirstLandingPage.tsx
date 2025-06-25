b99fb2aefeff2327ae459d7f15e6f6aac95d1b30
junxuanwu <18jw24@queensu.ca>
2025-06-25 10:22:06 -0400

    merge the router logic

--- client/src/components/FirstLandingPage.tsx
+++ client/src/components/FirstLandingPage.tsx
@@ -1,21 +1,23 @@
 import type { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
+import heroImg from '../assets//Animation - 1750629413888.gif'
 import './FirstLandingPage.css'
-import heroImg from '../assets/Animation - 1750629413888.gif'
 
-interface FirstLandingPageProps {
-  onStart: () => void
-}
+const FirstLandingPage: FC = () => {
+  const navigate = useNavigate()
 
-const FirstLandingPage: FC<FirstLandingPageProps> = ({ onStart }) => {
   return (
-    <div className='FirstLandingPageContainer'>
-      <img src={heroImg} alt="Hero Image of the first landing page" className='heroImg' />
+    <div className="FirstLandingPageContainer">
+      <img src={heroImg} alt="Hero" className="heroImg" />
       <h1 className="FirstLandingTitle">Welcome to NutriSwap</h1>
-
-      <p className="FirstLandingSubtitle">Helping you shop smarter and eat better — one grocery trip at a time.</p>
-
-      <button className="FirstLandingButton" onClick={onStart}>
-        Scan your groceries
+      <p className="FirstLandingSubtitle">
+        Helping you shop smarter and eat better — one grocery trip at a time.
+      </p>
+      <button
+        className="FirstLandingButton"
+        onClick={() => navigate('/upload')}
+      >
+        Scan your bill now
       </button>
     </div>
   )
