b3d288f16c6b007e11bf3d54753e831b637f2bb9
María Victoria Doll <mvictoriadoll@gmail.com>
2025-06-25 12:02:22 +0200

    add Get Feedback button and improve comments

--- client/src/components/Dashboard/SummaryList.tsx
+++ client/src/components/Dashboard/SummaryList.tsx
@@ -8,6 +8,7 @@ type Props = {
   goodNutriScorePercentage: NutritionSummary['goodNutriScorePercentage'];
 };
 
+//shows a breakdown of food categories with icons and percentages
 export default function SummaryList (props:Props){
   const { 
   freshFoodsPercentage, 
@@ -17,27 +18,27 @@ export default function SummaryList (props:Props){
   
    const items = [
     {
-      label: '% fresh food',
+      label: '% Fresh Food',
       value: freshFoodsPercentage,
-      icon:'🥦',
+      iconClass: 'fa-solid fa-apple-whole',
       color:"#22c55e",
     },
     {
-      label: '% high sugar item',
+      label: '% High Sugar Item',
       value: highSugarItemsPercentage,
-      icon:'🍭',
+      iconClass:'fa-solid fa-ice-cream',
       color:"#f87171",
     },
     {
-      label: '% processed food',
+      label: '% Processed Food',
       value: processedFoodPercentage,
-      icon:'🍔',
+      iconClass:'fa-solid fa-burger',
       color:"#facc15",
     }, 
       {
-      label: '%Nutri-score',
+      label: '% Nutri-score',
       value: goodNutriScorePercentage,
-      icon:'✅',
+      iconClass: 'fas fa-check-circle',
       color:"#4ade80",
     },
 
@@ -47,8 +48,9 @@ export default function SummaryList (props:Props){
     <ul className="summary-list">
     {items.map((item) => (
       <li key={item.label} className="summary-item">
+         {/* Colored icon */}
         <span className="icon" style={{ backgroundColor: item.color}}>
-          {item.icon}
+            <i className={item.iconClass}></i>
         </span>
         <span className="label">{item.label}</span>
         <span className="value">{item.value}%</span>
